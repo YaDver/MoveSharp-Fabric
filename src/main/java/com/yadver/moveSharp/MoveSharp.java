daee82d7b8297894a606366f204cf5c007c29bfa
YaDver <erik.denis.bauer@mail.ru>
2025-07-28 21:02:32 +0300

    Add Crawling using Mixin

--- src/main/java/com/yadver/moveSharp/MoveSharp.java
+++ src/main/java/com/yadver/moveSharp/MoveSharp.java
@@ -2,15 +2,7 @@ package com.yadver.moveSharp;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
-import net.minecraft.block.BlockState;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityDimensions;
-import net.minecraft.entity.EntityPose;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.Box;
-import net.minecraft.util.math.Vec3d;
 
 public class MoveSharp implements ModInitializer {
     public static final String MOD_ID = "move-sharp";
--- src/main/java/com/yadver/moveSharp/mixin/playerCrawlingServer.java
+++ src/main/java/com/yadver/moveSharp/mixin/playerCrawlingServer.java
@@ -1,8 +1,6 @@
 package com.yadver.moveSharp.mixin;
 
 import com.yadver.moveSharp.MoveSharp;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 import net.minecraft.entity.EntityPose;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
@@ -13,7 +11,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-//@Environment(EnvType.CLIENT)
 @Mixin(PlayerEntity.class)
 public abstract class playerCrawlingServer extends LivingEntity {
     protected playerCrawlingServer(EntityType<? extends LivingEntity> entityType, World world) {
